d3a4b7b060ac2d0bbe65f6615fdb5933340c4bd4
Henry Milner <henry@conviva.com>
2013-05-07 01:55:03 -0700

    Attempt to fix an issue in collectFuture().

--- src/main/scala/blinkdb/util/FutureRddOps.scala
+++ src/main/scala/blinkdb/util/FutureRddOps.scala
@@ -3,6 +3,8 @@ import spark.RDD
 import akka.dispatch.Future
 import akka.dispatch.Dispatcher
 import akka.dispatch.ExecutionContext
+import SparkUtils._
+import spark.SparkEnv
 
 object FutureRddOps {
   implicit def toFutureRddOps[I](rdd: RDD[I]) = new FutureRddOps(rdd)
@@ -10,14 +12,28 @@ object FutureRddOps {
 
 class FutureRddOps[I](rdd: RDD[I]) {
   def collectFuture()(implicit ec: ExecutionContext): Future[Array[I]] = {
-    Future({ rdd.collect() })
+    //A workaround for SPARK-534:
+    // https://spark-project.atlassian.net/browse/SPARK-534
+    val env = SparkEnv.get
+    Future({
+      SparkEnv.set(env)
+      rdd.collect()
+    })
   }
   
   def reduceFuture(reducer: (I, I) => I)(implicit ec: ExecutionContext): Future[I] = {
-    Future({ rdd.reduce(reducer) })
+    val env = SparkEnv.get
+    Future({
+      SparkEnv.set(env)
+      rdd.reduce(reducer)
+    })
   }
   
   def countFuture()(implicit ec: ExecutionContext): Future[Long] = {
-    Future({ rdd.count() })
+    val env = SparkEnv.get
+    Future({
+      SparkEnv.set(env)
+      rdd.count()
+    })
   }
 }
